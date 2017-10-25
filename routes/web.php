0163fef0cb85e1d8cccd4f82007b54e9acae332d
S3A432087 陳人傑 <h220641963@gmail.com>
2017-10-25 19:07:36 +0800

    刪除該任務

--- routes/web.php
+++ routes/web.php
@@ -37,6 +37,8 @@ Route::post('/task', function (Request $request) {
     /**
      * 刪除任務
      */
-    Route::delete('/task/{task}', function (Task $task) {
-        //
-    });
+Route::delete('/task/{task}', function (Task $task) {
+    $task->delete();
+
+    return redirect('/');
+});
